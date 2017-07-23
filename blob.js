4b8641bb18ab193226901fad32ed93b6c1d7a48e
stobe97 <30377006+stobe97@users.noreply.github.com>
2017-07-22 21:12:59 -0700

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,9 +1,9 @@
-function blob() {
+function Blob() {
 	this.pos = createVector(width/2, height/2);
 	this.r = 64;
 
 	this.show = function() {
 		fill(255);
-		ellipse(this.x, this.y, this.r*2, this.r*2);
+		ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
 	}
 }
